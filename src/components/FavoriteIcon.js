d106fb2d59662700b7aa0f791f98179a61330ccf
dom <crhicko@protonmail.com>
2022-01-06 18:23:33 -0500

    update func call to trigger onclick not on update

--- src/components/FavoriteIcon.js
+++ src/components/FavoriteIcon.js
@@ -1,35 +1,38 @@
-import useDidUpdateEffect from "../util/useDidUpdateEffect"
+import PropTypes from 'prop-types'
 
 const FavoriteIcon = ( {isFavorite, setFavorite, fly_id} ) => {
 
     const toggleFavorite = () => {
+        updateFavorite(!isFavorite)
         setFavorite(!isFavorite)
     }
 
-
-    useDidUpdateEffect(() => {
-        const updateFavorite = async() => {
-            const res = await fetch('http://localhost:4000/flies/' + fly_id + '/favorite', {
-                method: 'POST',
-                credentials: 'include',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    set_favorite: isFavorite
-                })
+    const updateFavorite = async(fav) => {
+        const res = await fetch('http://localhost:4000/flies/' + fly_id + '/favorite', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                set_favorite: fav
             })
-            console.log(await res.json())
-        }
-        updateFavorite()
-    },[isFavorite])
+        })
+        console.log(await res.json())
+    }
 
     return (
         <div>
             {isFavorite ? <i className="fas fa-star" onClick={toggleFavorite}/> : <i className="far fa-star" onClick={toggleFavorite}/>}
         </div>
     )
+}
+
+FavoriteIcon.propTypes = {
+    isFavorite: PropTypes.bool.isRequired,
+    setFavorite: PropTypes.func.isRequired,
+    fly_id: PropTypes.number.isRequired
 }
 
 export default FavoriteIcon
