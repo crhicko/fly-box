7e3b69d5119cdb4e12bd27a4103bd2b03df092a9
dom <crhicko@protonmail.com>
2022-01-09 18:50:29 -0500

    change some styling to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,21 +16,25 @@ const Header = ({toggleLoginModal}) => {
 
     return (
         <header>
-            {/* <img className="logo" src="http://localhost:3000/src/components/Header/3x.png" alt="logo"/> */}
+            <div>
+                <img className="logo" src="http://localhost:3000/src/components/Header/3x.png" alt="logo"/>
+            </div>
             <nav>
                 <ul className="nav_buttons">
                     <li><Link to="/flies">Flies</Link></li>
                     <li><Link to="/materials">{user?.username}</Link></li>
                     <li><Link to="/home">Home</Link></li>
                     <li><a href="s">Kit Builder</a></li>
-                    {user ?
-                        <button onClick={() => {logoutUser()}}>Logout</button>
-                    :
-                    <button onClick={() => {
-                        toggleLoginModal()
-                    }}>Login</button>}
                 </ul>
             </nav>
+            <div className="right-button-box">
+                {user ?
+                            <button onClick={() => {logoutUser()}}>Logout</button>
+                        :
+                        <button onClick={() => {
+                            toggleLoginModal()
+                        }}>Login</button>}
+            </div>
         </header>
 
     )
