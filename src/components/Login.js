0fb014957460c13db691c2ff948391284fccd756
dom <crhicko@protonmail.com>
2022-01-18 13:19:01 -0500

    add registration form to the login form, error handling for registration

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useContext } from 'react'
 import { useNavigate } from 'react-router'
 import { UserContext } from '../context/UserContext'
 import { useFormik } from 'formik'
-import { validateUserName, validateLoginPassword } from '../util/Validator'
+import { validateUserName, validateLoginPassword, validateEmail, validateRegistrationPassword } from '../util/Validator'
 import './Login.css'
 
 const Login = () => {
@@ -20,37 +20,24 @@ const Login = () => {
     const validate = values => {
         let errors = {}
         errors = {...validateUserName(values.username), ...errors}
-        errors = {...validateLoginPassword(values.password), ...errors}
+        if(!isLoginForm)
+            errors = {...validateEmail(values.email), ...errors}
+        errors = isLoginForm ? {...validateLoginPassword(values.password), ...errors} : {...validateRegistrationPassword(values.password, values.confirm_password), ...errors}
         return errors
     }
 
     const formik = useFormik({
         initialValues: {
             username: '',
-            password: ''
+            password: '',
+            confirm_password: '',
+            email: ''
         },
         onSubmit: values => {
-            loginUser(values.username, values.password)
+            isLoginForm ? loginUser(values.username, values.password) : registerUser(values.username, values.password, values.email)
         },
         validate
     })
-
-    //Registration
-    //TODO: Password rules validation
-    //TODO: Verify Password
-    //TODO: Check for email
-    //TODO: Check for username
-    //TODO: collapse animation and form swap on reg/login swap
-
-    useEffect(() => {
-        if (verifyPassword === '')
-            return
-        // console.log(password + " " + verifyPassword)
-        if (password === verifyPassword)
-            setIsMatchingPassword(true)
-        else
-            setIsMatchingPassword(false)
-    }, [verifyPassword, password]);
 
     const loginUser = async (user, pass) => {
         setFetchError(undefined)
@@ -75,13 +62,48 @@ const Login = () => {
         else {
             setFetchError(data.message);
         }
+    }
 
+    const registerUser = async (user, pass, email) => {
+        const res = await fetch('http://localhost:4000/register', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: user,
+                email: email,
+                password: pass,
+            })
+        })
+        const data = await res.json();
+        console.log(data)
+        if (data.id) {
+            setUser(data)
+            navigate('/flies');
+        }
+        else {
+            setFetchError(data.message);
+        }
+    }
+
+    const toggleLoginForm = () => {
+        setIsLoginForm(!isLoginForm)
     }
 
     return (
         <div style={{ textAlign:'center'}}>
             <div className="rounded-box" style={{width:"max(25vw,300px)", display: "inline-block"}}>
                 <form onSubmit={formik.handleSubmit}>
+                    {!isLoginForm && <div className="form-row">
+                        <h3>Email</h3>
+                        <input id="email" name="email" type="text" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email}/>
+                        {formik.touched.email && formik.errors.email ? (
+                            <div className='form-error'>{formik.errors.email}</div>
+                        ): null}
+                    </div>}
                     <div className="form-row">
                         <h3>Username</h3>
                         <input id="username" name="username" type="text" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.username}/>
@@ -95,9 +117,19 @@ const Login = () => {
                         {formik.touched.password && formik.errors.password ? (
                             <div className='form-error'>{formik.errors.password}</div>
                         ): null}
-
                     </div>
-                    <button type="submit">Submit</button>
+                    {!isLoginForm && <div className="form-row">
+                        <h3>Confirm Password</h3>
+                        <input id="confirm_password" name="confirm_password" type="password" onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.confirm_password}/>
+                        {formik.touched.confirm_password && formik.errors.confirm_password ? (
+                            <div className='form-error'>{formik.errors.confirm_password}</div>
+                        ): null}
+                    </div>}
+                    <div className='form-row' >
+                        <button style={{}} type="submit">Submit</button>
+                        <br></br>
+                        <a className='change-form' onClick={toggleLoginForm}>{isLoginForm ? "Don't Have an Account? Sign Up Here!" : "I Have an Account"}</a>
+                    </div>
                     {fetchError ? <div className='form-error'>{fetchError}</div> : null}
                 </form>
             </div>
--- src/util/Validator.js
+++ src/util/Validator.js
@@ -12,7 +12,7 @@ export const validateEmail = (email) => {
 
     if (!email)
         errs.email = 'Required';
-    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email))
+    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email))
         errs.email = 'Invalid Email Address';
 
     return errs;
@@ -26,13 +26,17 @@ export const validateLoginPassword = (password) => {
     return errs;
 }
 
-export const validateRegistrationPassword = (password) => {
+export const validateRegistrationPassword = (password, confirm_password) => {
     const errs = {}
 
     if (!password)
         errs.password = 'Required';
-    if (password.length < 5)
+    else if (password.length < 5)
         errs.password = 'Password must be at least 5 characters long';
+    if (!confirm_password)
+        errs.confirm_password = 'Required';
+    else if (password != confirm_password)
+        errs.confirm_password = 'Passwords do not match'
 
     return errs;
 }
