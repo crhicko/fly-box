d363abda1854d77f09c560ec3c811e9c0deada2b
dom <crhicko@protonmail.com>
2022-02-21 16:57:44 -0500

    Remove user check from landing, will maybe redirect to flies if a user is already logged in

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -6,15 +6,13 @@ const LandingPage = () => {
     const navigate = useNavigate();
 
     return (
-        <>
-            <section className='splash wave-bg'>
-                <h1 style={{fontSize: '2rem', marginBottom:'1rem'}}>Find Flies, Learn to Tie, Catch Fish</h1>
-                <div className='button-box'>
-                    <button className='btn text-large' onClick={() => navigate('/login')}>Sign Up</button>
-                    <button className='btn text-large' onClick={() => navigate('/flies')}>View Flies</button>
-                </div>
-            </section>
-        </>
+        <section className='splash wave-bg'>
+            <h1 style={{fontSize: '2rem', marginBottom:'1rem'}}>Find Flies, Learn to Tie, Catch Fish</h1>
+            <div className='button-box'>
+                <button className='btn text-large' onClick={() => navigate('/login')}>Log In</button>
+                <button className='btn text-large' onClick={() => navigate('/flies')}>View Flies</button>
+            </div>
+        </section>
     )
 }
 
